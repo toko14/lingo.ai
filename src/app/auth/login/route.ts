9fa6a8d474b18225d23d4911cdee226298887c35
Toko <tk.5229@icloud.com>
2024-10-26 17:28:16 +0900

    WIP

--- src/app/auth/login/route.ts
+++ src/app/auth/login/route.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
-  const requestUrl = new URL(request.url)
+  //const requestUrl = new URL(request.url)
   const formData = await request.json()
   const email = formData.email
   const password = formData.password
